ec40a821530dc4bd23746e8bff9b46197206b245
Alessandro Mauro Incantalupo <alessandro.incantalupo@gmail.com>
2026-03-07 15:55:51 +0100

    fix: proper agnostic load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ApplicationRef, DoBootstrap, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
-import { appConfig } from './app.config';
+import { routes } from './app.routes';
 
 @NgModule({
-  imports: [BrowserModule, AppComponent],
-  providers: [...appConfig.providers],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes, { bindToComponentInputs: true }),
+    AppComponent,
+  ],
 })
 export class AppModule implements DoBootstrap {
   ngDoBootstrap(appRef: ApplicationRef) {
