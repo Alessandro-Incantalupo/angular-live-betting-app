7f0ab9217371078693654e5174ef77af1cd0c115
Alessandro Mauro Incantalupo <alessandro.incantalupo@gmail.com>
2026-03-07 15:45:20 +0100

    feat: app module agnostic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { ApplicationRef, DoBootstrap, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { appConfig } from './app.config';
 
 @NgModule({
   imports: [BrowserModule, AppComponent],
+  providers: [...appConfig.providers],
 })
 export class AppModule implements DoBootstrap {
   ngDoBootstrap(appRef: ApplicationRef) {
